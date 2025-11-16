2e6596149fbc4b71681b3d3540c88033ccab274c
IamSubarist <optimistroma927@gmail.com>
2025-11-16 20:30:48 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Это простое PWA</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+// Регистрация Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("Service Worker зарегистрирован:", registration);
+      })
+      .catch((error) => {
+        console.log("Ошибка регистрации Service Worker:", error);
+      });
+  });
+}
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
